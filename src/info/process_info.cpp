c39c48b8098abdde3be3de5b648f7805b4a0cc5a
npohl <npohl@mymail.mines.edu>
2017-02-15 19:52:41 -0700

    ProcessInfo is now all passing.

--- src/info/process_info.cpp
+++ src/info/process_info.cpp
@@ -8,22 +8,49 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <algorithm>
 
 using namespace std;
 
 /**
- * Returns a Process instance representing the process with the given process
- * ID (pid). The data for the process should be loaded from the directory formed
- * by joining the given basedir with the process ID (basedir/pid).
+ * This function is used to locate all pids inside the basedirectory given to it.
+ * Most uses should be for either locating threads within a process, or finding all
+ * processes in proc.
  */
+vector<int> getAllPids(const char* basedir) {
+    vector<int> pids;
+    // these are variables to store directory info
+    DIR *dir;
+    struct dirent *ent;
+
+    // open the directory
+    dir = opendir(basedir);
+
+    if (!dir) {
+        //cout << "Couldn't open directory\n";
+        return pids;
+    }
+
+    while ((ent = readdir(dir))) {
+        char* dirName = ent->d_name; 
+
+        // only want to count directories if they have digits (these are pids)
+        if (isdigit(dirName[0])) {
+            // convert to int
+            int pid = atoi(dirName);
+
+            pids.push_back(pid);
+        }
+    }
+
+    closedir(dir);
+
+    return pids;
+
+}
+
+
 ProcessInfo get_process(int pid, const char* basedir) {
-    /**
-     * So far, found three files we need. Still missing data
-     * on cpu_percent and threads.
-     *
-     * Should probably start by creating 3 file io, checking to see if all exist
-     * if one doesn't return ProcessInfo(). If they all pass create the construct
-     */
 
     char pidString [10];
     snprintf(pidString, sizeof(pidString), "%d", pid);
@@ -34,11 +61,11 @@ ProcessInfo get_process(int pid, const char* basedir) {
     strcat(memString, pidString);
     strcat(memString, "/statm");
 
-    char cpuSetString[50];
-    strcpy(cpuSetString, basedir);
-    strcat(cpuSetString, "/");
-    strcat(cpuSetString, pidString);
-    strcat(cpuSetString, "/cpuset");
+    char commandString[50];
+    strcpy(commandString, basedir);
+    strcat(commandString, "/");
+    strcat(commandString, pidString);
+    strcat(commandString, "/cmdline");
 
     char statString[50];
     strcpy(statString, basedir);
@@ -47,22 +74,23 @@ ProcessInfo get_process(int pid, const char* basedir) {
     strcat(statString, "/stat");
 
     ifstream memStat(memString);
-    ifstream cpuSet(cpuSetString);
+    ifstream commandLine(commandString);
     ifstream stat(statString);
     
-    if (memStat.fail() || cpuSet.fail() || stat.fail()) {
-        cout << "Couldn't find the file\n";
+    if (memStat.fail() || commandLine.fail() || stat.fail()) {
+        //cout << "Couldn't find the file\n";
         return ProcessInfo();
     }
     
     else {
 
         //turn on to check for segfault
-        return ProcessInfo();
+        //return ProcessInfo();
+
+        //cout << "Just started the process\n\n\n";
 
         stringstream memStatStream;
         string line;
-        int value;
         struct ProcessInfo process;
 
         //Start with statm file
@@ -70,39 +98,26 @@ ProcessInfo get_process(int pid, const char* basedir) {
         getline(memStat, line);
         memStatStream << line;
 
-        memStatStream >> value;
-        process.size = value;
-
-        memStatStream >> value;
-        process.resident = value;
-
-        memStatStream >> value;
-        process.share = value;
-
-        memStatStream >> value;
-        process.trs = value;
-
-        memStatStream >> value;
-        process.lrs = value;
-
-        memStatStream >> value;
-        process.drs = value;
-
-        memStatStream >> value;
-        process.dt = value;
+        memStatStream >> process.size;
+        memStatStream >> process.resident;
+        memStatStream >> process.share;
+        memStatStream >> process.trs;
+        memStatStream >> process.lrs;
+        memStatStream >> process.drs;
+        memStatStream >> process.dt;
         
         //end statm
         
 
         //comes from input
         process.pid = pid;
+
+        //cout << "Finished statm, size = " << process.size << "\n\n\n";
         
 
         //is a string
         //is the whole file pid/cpuset
-        getline(cpuSet, process.command_line);
-        //process.command_line = line;
-
+        getline(commandLine, process.command_line);
 
         //The rest of the values are in stat
         //this file is one line
@@ -110,14 +125,13 @@ ProcessInfo get_process(int pid, const char* basedir) {
         getline(stat, line);
         processStream << line;
 
-        processStream >> value;
-        process.tgid = value;
+        processStream >> process.tgid;
 
-        string commValue;
-        processStream >> commValue;
+        //string commValue;
+        processStream >> process.comm;
         //is a char array
         //whole file pid/comm
-        strcpy(process.comm, commValue.c_str());
+        //strcpy(process.comm, commValue.c_str());
 
         uint8_t stateVal;
         //string stateVal;
@@ -130,95 +144,86 @@ ProcessInfo get_process(int pid, const char* basedir) {
         //process.state = 82;
         process.state = stateVal; //uint8_t version
 
-        processStream >> value;
-        process.ppid = value;
-        processStream >> value;
-        process.pgrp = value;
-        processStream >> value;
-        process.session = value;
-        processStream >> value;
-        process.tty_nr = value;
-        processStream >> value;
-        process.tpgid = value;
-        processStream >> value;
-        process.flags = value;
-        processStream >> value;
-        process.minflt = value;
-        processStream >> value;
-        process.cminflt = value;
-        processStream >> value;
-        process.majflt = value;
-        processStream >> value;
-        process.cmajflt = value;
-        processStream >> value;
-        process.utime = value;
-        processStream >> value;
-        process.stime = value;
-        processStream >> value;
-        process.cutime = value;
-        processStream >> value;
-        process.cstime = value;
-        processStream >> value;
-        process.priority = value;
-        processStream >> value;
-        process.nice = value;
-        processStream >> value;
-        process.num_threads = value;
-        processStream >> value;
-        process.itrealvalue = value;
-        processStream >> value;
-        process.starttime = value;
-        processStream >> value;
-        process.vsize = value;
-        processStream >> value;
-        process.rss = value;
-        processStream >> value;
-        process.rsslim = value;
-        processStream >> value;
-        process.startcode = value;
-        processStream >> value;
-        process.endcode = value;
-        processStream >> value;
-        process.startstack = value;
-        processStream >> value;
-        process.kstkesp = value;
-        processStream >> value;
-        process.kstkeip = value;
-        processStream >> value;
-        process.signal = value;
-        processStream >> value;
-        process.blocked = value;
-        processStream >> value;
-        process.sigignore = value;
-        processStream >> value;
-        process.sigcatch = value;
-        processStream >> value;
-        process.wchan = value;
-        processStream >> value;
-        process.nswap = value;
-        processStream >> value;
-        process.cnswap = value;
-        processStream >> value;
-        process.exit_signal = value;
-        processStream >> value;
-        process.processor = value;
-        processStream >> value;
-        process.rt_priority = value;
-        processStream >> value;
-        process.policy = value;
-        processStream >> value;
-        process.delayacct_blkio_ticks = value;
-        processStream >> value;
-        process.guest_time = value;
-        processStream >> value;
-        process.cguest_time = value;
+        processStream >> process.ppid;
+        processStream >> process.pgrp;
+        processStream >> process.session;
+        processStream >> process.tty_nr;
+        processStream >> process.tpgid;
+        processStream >> process.flags;
+        processStream >> process.minflt;
+        processStream >> process.cminflt;
+        processStream >> process.majflt;
+        processStream >> process.cmajflt;
+        processStream >> process.utime;
+        processStream >> process.stime;
+        processStream >> process.cutime;
+        processStream >> process.cstime;
+        processStream >> process.priority;
+        processStream >> process.nice;
+        processStream >> process.num_threads;
+        processStream >> process.itrealvalue;
+        processStream >> process.starttime;
+        processStream >> process.vsize;
+        processStream >> process.rss;
+        processStream >> process.rsslim;
+        processStream >> process.startcode;
+        processStream >> process.endcode;
+        processStream >> process.startstack;
+        processStream >> process.kstkesp;
+        processStream >> process.kstkeip;
+        processStream >> process.signal;
+        processStream >> process.blocked;
+        processStream >> process.sigignore;
+        processStream >> process.sigcatch;
+        processStream >> process.wchan;
+        processStream >> process.nswap;
+        processStream >> process.cnswap;
+        processStream >> process.exit_signal;
+        processStream >> process.processor;
+        processStream >> process.rt_priority;
+        processStream >> process.policy;
+        processStream >> process.delayacct_blkio_ticks;
+        processStream >> process.guest_time;
+        processStream >> process.cguest_time;
         //end of pid/stat file
 
-        //threads is a vector of processInfos
-        //not sure why it is processInfo
-        vector<ProcessInfo> processThreads;
-        processThreads.push_back(ProcessInfo());
-        process.threads = processThreads;
+        char taskString[50];
+        strcpy(taskString, basedir);
+        strcat(taskString, "/");
+        strcat(taskString, pidString);
+        strcat(taskString, "/task");
+
+        //cout << "starting threads, task is in this state: " << process.state << "\n\n\n";
+        process.threads = get_all_processes(taskString);
+
+        //all thread group ids are equal to the process pid
+        for (int i = 0; i < process.threads.size(); i++) {
+            process.threads[i].tgid = process.pid; 
+        }
+
+        //need to clean the command_line up
+        //if it is empty, we need to use comm without parenthesis
+        //if it is not empty, we need to get rid of the null characters
+        if (process.command_line == "") {
+            string str(process.comm);
+
+            //cout << "command_line before: " << str << endl;
+
+            size_t i = 0;
+            while (i < str.size()) {
+                if (str[i] == '(' || str[i] == ')') str.erase(i,1);
+                else ++i;
+            }
+
+            //cout << "command line after: " << str << endl;
+    
+            process.command_line = str;
+        }
+        else {
+            // change weird null characters (\0) in the cmdline file to spaces.
+            replace(process.command_line.begin(), process.command_line.end(), '\0', ' ');
+            process.command_line = process.command_line.substr(0, process.command_line.size() - 1);
+        }
 
         /**
          * The percentage of a single CPU used by this process over some period. You
@@ -226,6 +231,8 @@ ProcessInfo get_process(int pid, const char* basedir) {
          * different points in time.
          */
         process.cpu_percent = 0;
+
+        return process;
 
 
     }
@@ -240,6 +247,17 @@ ProcessInfo get_process(int pid, const char* basedir) {
  * /proc/<pid>/task basedir).
  */
 vector<ProcessInfo> get_all_processes(const char* basedir) {
-    // TODO: implement me
-    return vector<ProcessInfo>();
+    
+    vector<int> pids = getAllPids(basedir);
+
+    vector<ProcessInfo> allProcesses;
+    ProcessInfo process;
+    
+    for (int i = 0; i < pids.size(); i++) {
+        //cout << "calling get process\n\n\n";
+        process = get_process(pids[i], basedir);
+        allProcesses.push_back(process);
+    }
+
+    return allProcesses;
 }
